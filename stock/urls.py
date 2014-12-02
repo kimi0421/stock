0f03665d11a2b4a9339809aa95f7cdd4243b1f38
Kimi Jin <jinyinzhen@gmail.com>
2014-12-01 22:18:40 -0800

    change the url

--- stock/urls.py
+++ stock/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from dashboard.views import DashboardView
 
 urlpatterns = patterns('',
@@ -10,3 +11,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'stock/', DashboardView.as_view()),
 )
+
+urlpatterns += staticfiles_urlpatterns()
